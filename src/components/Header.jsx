cdd2485b999e5022d446443a42e1ca44a927c7ef
wbsantiago <will.jimmy27@hotmail.com>
2022-09-27 21:55:39 -0300

    starting new project meme generator at course of react in scrimba plataform

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import trollFace from "../assets/troll-face.svg"
+import './header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="div-logo">
+                <a href="#" className="header-logo"><img src={trollFace} alt="logo" /></a>
+                <span className="header--title">Meme Generator</span>
+            </div>
+            <p className="header--description">React Course - Project 3</p>
+        </div>
+    )
+}
