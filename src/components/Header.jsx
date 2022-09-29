76732a1fd3fbd3ec683e4efe4fcc3090781dd6dd
wbsantiago <will.jimmy27@hotmail.com>
2022-09-28 23:21:31 -0300

    changes at tag to be more semantic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import './header.css'
 
 export default function Header() {
     return (
-        <div className="header">
+        <header className="header">
             <div className="div-logo">
                 <a href="#" className="header-logo"><img src={trollFace} alt="logo" /></a>
                 <span className="header--title">Meme Generator</span>
             </div>
             <p className="header--description">React Course - Project 3</p>
-        </div>
+        </header>
     )
 }
