7879dfba7176abad63ebbfabdd8d97d96490eaf4
wbsantiago <will.jimmy27@hotmail.com>
2022-10-02 23:28:51 -0300

    use useState to setup our MainContent

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,16 +1,48 @@
-import './main-content.css'
+import React from "react"
+import memesData from "../memesData.js"
 
-export default function MainContent() {
+export default function Meme() {
+    
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
+    
     return (
         <main>
-            <form>
-                <div className="form--input">
-                <label><input type="text" name="" id="" placeholder="Shut up" /></label>
-                <label><input type="text" name="" id="" placeholder="and take my money" /></label>
-                </div>
-                <button type="submit">Get a new meme image 🖼</button>
-                <img src="https://s3-alpha-sig.figma.com/img/56bc/a2dd/d69146f4b0afd296630ab1e3ba14c471?Expires=1665360000&Signature=eVBy0iSoO8LwxezgIn5qN8nFjGHNKiMxCi3oV71Z0q~AIa~CfAyaS8vsPwHDvW6gmVVGh2Humo6IjLP7nAz9CAoB6ZhB9B5ikYQfLBMqY02UNSNnThmqmbtftrSZK6pvUy6EaLV4w6F9~6fJJICT523eW32xfMOW~o-31JFydtLjdAAR4BBmJgwE-vTodgrHnNdrPeayGQBHoksnhXSpW4VVU-EgbNDSkewUrcnlo0-6NZTq9BZ8uEq5doK25X6MeWjWunRj3KzBG2q5nO12fF18P~Zdswt5RYrkqTq-13cLEa7LxHCeNjPt5S47x79Xghuqi4oO7y9nU-gcwZpKlA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt="" className="main-image" />
-            </form>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <img src={memeImage} className="meme--image" />
         </main>
     )
 }
