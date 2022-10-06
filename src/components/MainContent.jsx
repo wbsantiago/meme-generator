7fb47d81c8ab118d29f9cb8ddfbfa4d256c1a899
wbsantiago <will.jimmy27@hotmail.com>
2022-10-06 18:49:10 -0300

    added name, value and onChange at input and its working the top and bottom text

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -23,6 +23,14 @@ export default function Meme() {
         
     }
     
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return (
         <main>
             <div className="form">
@@ -31,11 +39,17 @@ export default function Meme() {
                         type="text"
                         placeholder="Top text"
                         className="form--input"
+                        name="topText"
+                        value={meme.topText}
+                        onChange={handleChange}
                     />
                     <input
                         type="text"
                         placeholder="Bottom text"
                         className="form--input"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="form--button__div">
@@ -47,7 +61,11 @@ export default function Meme() {
                     </button>
                 </div>
             </div>
-            <img src={meme.randomImage} className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
