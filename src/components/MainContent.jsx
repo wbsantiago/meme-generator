c00c6c9bf54f7d1a0e4de68f9c19d8b209e04735
wbsantiago <will.jimmy27@hotmail.com>
2022-10-10 00:44:57 -0300

    add async function at useEffect function

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -13,9 +13,12 @@ export default function Meme() {
     
     
     React.useEffect(() => {
-        fetch(`https://api.imgflip.com/get_memes`)
-            .then(res => res.json())
-            .then(data => setAllMemeImages(data.data.memes))
+        async function getMemes() {
+            const res = await fetch(`https://api.imgflip.com/get_memes`)
+            const data = await res.json()
+            setAllMemeImages(data.data.memes)
+        }
+        getMemes()
     }, [])
     
         
